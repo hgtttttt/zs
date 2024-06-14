dbbe9b77eca3c63045e7c013f2b2b82de6c2c90d
hgtttttt <2842938528@qq.com>
2024-06-14 17:46:16 +0800

    init

--- infer.py
+++ infer.py
@@ -3,11 +3,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def main():
 	model, tokenizer = AutoModelForCausalLM.from_pretrained("/share_data/base_model"), AutoTokenizer.from_pretrained("/share_data/base_model")
 	model.eval()
-	text = input("Enter text: ")
-	encoded_input = tokenizer(text, return_tensors="pt")
-	output = model.generate(**encoded_input, max_length=50, num_return_sequences=5, temperature=0.9)
-	for i, sample_output in enumerate(output):
-		print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+	while True:
+		text = input("Enter text: ")
+		encoded_input = tokenizer(text, return_tensors="pt")
+		output = model.generate(**encoded_input, max_length=100, num_return_sequences=3, temperature=0.9)
+		for i, sample_output in enumerate(output):
+			print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
 
 if __name__ == '__main__':
 	main()
